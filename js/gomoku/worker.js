10d3e2a0cdcdb6ccc5adb8a86441819d4618aa31
wei yijun <2o18o424@gmail.com>
2020-12-10 23:55:05 +0800

    Gomoku: use Promise to communicate with Web workers & GUI Part I

--- js/gomoku/worker.js
+++ js/gomoku/worker.js
@@ -1,6 +1,9 @@
 importScripts("/js/gomoku/gomoku.js");
 
 let send=Module.cwrap("handler",null,["string"]);
+Module.onRuntimeInitialized=()=>{
+	self.postMessage("");
+};
 self.addEventListener("message",e=>{
 	send(e.data);
 	self.postMessage("");
